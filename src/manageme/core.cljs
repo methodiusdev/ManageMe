ece58e7d1ebed3902fe1bccad848427f9a36a640
methodiusdev <methodius.dev@gmail.com>
2026-06-15 22:02:32 +0200

    Basic frontend for projects.

--- src/manageme/core.cljs
+++ src/manageme/core.cljs
@@ -1,15 +1,13 @@
 (ns manageme.core
   (:require [reagent.core :as r]
             [reagent.dom.client :as rdom]
-            [manageme.state :as state]))
+            [manageme.views :as v]))
 
 (defonce react-root
   (rdom/create-root (.getElementById js/document "app")))
 
 (defn app []
-  [:div
-   [:h1 "ManageMe - powered by Clojure"]
-   [:p (str "Total projects: " (count (:projects @state/app-state)))]])
+  [v/main-view])
 
 (defn ^:dev/after-load reload []
   (rdom/render react-root [app]))
