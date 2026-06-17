d397432bc259faf52b0a5dd1bab25fbdebc335c6
methodiusdev <methodius.dev@gmail.com>
2026-06-17 21:00:21 +0200

    Implement header.

--- src/manageme/views.cljs
+++ src/manageme/views.cljs
@@ -1,28 +1,22 @@
 (ns manageme.views
-  (:require [manageme.state :as state]))
+  (:require [reagent.core :as r]
+            [manageme.state :as state]))
 
-(defn project-form []
-  [:div
-   [:h3 "Add new project: "]
-   [:input {:type "text" :placeholder "Name..."}]
-   [:textarea {:placeholder "Description..."}]
-   [:button "Save"]])
-
-(defn project-list []
-  (let [projects (:projects @state/app-state)]
-    [:div
-     [:h3 "Projects"]
-     (if (empty? projects)
-       [:p "There are no projects avaliable. Create one!"]
-       [:ul
-        (for [project projects]
-          [:li {:key (:id project)}
-           [:strong (:title project)] " - " (:description project)])])]))
+(defn header-component []
+  [:header {:class "bg-white border-b border-gray-100 px-6 py-4 flex
+                    justify-between items-center max-w-5xl mx-auto w-full"}
+   [:h1 {:class "text-xl font-black text-emerald-800 tracking-tight
+                 flex items-center space-x-2"}
+    [:span "ManageMe -- Powered by Clojure"]]
+   [:div {:class "flex items-center space-x-3"}
+    [:span {:class "text-xs font-medium text-gray-400 bg-gray-50
+                    border border-gray-100 px-2.5 py-1 rounded-md"} 
+     "Kira's Workspace"]]])
 
 (defn main-view []
-  [:div {:syle {:max-width "800px" :margin "0 auto" :padding "20px"}}
-   [:h1 "ManageMe - powered by Clojure"]
-   [:hr]
-   [project-form]
-   [:hr]
-   [project-list]])
+  [:div {:class "min-h-screen bg-gray-50/50 flex flex-col font-sans antialiased"}
+   [header-component]
+   [:main {:class "p-6 max-w-5xl w-full mx-auto flex-1 mt-4"}
+    [:div {:class "text-center py-12"}
+     [:p {:class "text-sm text-gray-400 font-medium animate-pulse"} 
+      "Workspace shell active. Awaiting grid wrapper..."]]]])
